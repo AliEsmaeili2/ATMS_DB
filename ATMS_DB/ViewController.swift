57bcaad60c20a4f62cdeb097fca296de31c47134
Ali Esmaeili <ali.esmaeili1927@gmail.com>
2023-11-06 11:29:04 +0330

    Initial Commit

---
+++ ATMS_DB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ATMS_DB
+//
+//  Created by Ali Esmaeili on 11/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
