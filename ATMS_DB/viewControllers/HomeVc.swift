d8f1e44f62d3d9e13b58158fdbea27d8d0e8a585
Ali Esmaeili <ali.esmaeili1927@gmail.com>
2023-11-06 14:42:18 +0330

    1

--- ATMS_DB/viewControllers/HomeVc.swift
+++ ATMS_DB/viewControllers/HomeVc.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVc: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
