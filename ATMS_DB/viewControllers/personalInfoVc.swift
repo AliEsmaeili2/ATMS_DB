66732c4277ff361780d33d2b018fb68de7fa5f21
Ali Esmaeili <ali.esmaeili1927@gmail.com>
2023-11-13 19:00:38 +0330

    Add personalInfo page & flightInformation page

--- ATMS_DB/viewControllers/personalInfoVc.swift
+++ ATMS_DB/viewControllers/personalInfoVc.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import DropDown
+import SkyFloatingLabelTextField
 
 class personalInfoVc: UIViewController {
-
+    
+    @IBOutlet weak var fullName: SkyFloatingLabelTextField!
+    @IBOutlet weak var nationalCode: SkyFloatingLabelTextField!
+    @IBOutlet weak var phone: SkyFloatingLabelTextField!
+    @IBOutlet weak var BirthDayPicker: UIDatePicker!
+    
+    @IBOutlet weak var DDView: UIView!
+    @IBOutlet weak var DDLabel: UILabel!
+    
+    let dropDown = DropDown()
+    let genderArray = [" Male", " Female"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        DDLabel.text = " Select Gender 🔽"
+        
+        // UIView or UIBarButtonItem
+        dropDown.anchorView = DDView
+        dropDown.dataSource = genderArray
+        
+        // Top of drop down will be below the anchorView
+        dropDown.bottomOffset = CGPoint(x: 0, y:(dropDown.anchorView?.plainView.bounds.height)!)
+        dropDown.direction = .bottom
+        
+        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            print("Selected item: \(item) at index: \(index)")
+            self.DDLabel.text = genderArray[index]
+        }
+        
+        DropDown.appearance().selectedTextColor = .systemGreen
+        DropDown.appearance().backgroundColor = UIColor.white
+        //    DropDown.appearance().selectionBackgroundColor = UIColor.systemGray6
+        DropDown.appearance().cornerRadius = 2
     }
     
-
+    
+    @IBAction func DDButton(_ sender: Any) {
+        
+        dropDown.show()
+    }
+    
 }
