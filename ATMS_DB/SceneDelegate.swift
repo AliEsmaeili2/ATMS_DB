d2ab2e7bce8883a169d7d1e25131f0b3b67fa579
Ali Esmaeili <ali.esmaeili1927@gmail.com>
2023-12-26 13:13:50 +0330

    set lock Screen and Disable DarkMode in this App

--- ATMS_DB/SceneDelegate.swift
+++ ATMS_DB/SceneDelegate.swift
@@ -13,9 +13,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        // Override point for customization after application launch.
+         if let windowScene = scene as? UIWindowScene {
+             if #available(iOS 13.0, *) {
+                 // Override the user interface style to force light appearance
+                 windowScene.windows.first?.overrideUserInterfaceStyle = .light
+             }
+         }
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
